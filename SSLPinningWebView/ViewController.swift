2b81f3cd50eebdf3193953298fcba2595bca9113
Kuang-Yu <kuangyu.nien@somnics.com>
2023-12-05 17:04:32 +0800

    Move check 'serverTrust is valid' to a background thread

--- SSLPinningWebView/ViewController.swift
+++ SSLPinningWebView/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
     }
     private let targetHost1 = "https://www.google.com"
     private let targetHost2 = "https://medium.com"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         testURLSessionTask(urlString: targetHost)
-
+        
         showWebPage(urlString: targetHost)
     }
-
+    
     private func showWebPage(urlString: String) {
         if let url = URL(string: urlString) {
             let request = URLRequest(url: url)
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
             mWebView.navigationDelegate = self
         }
     }
-
+    
     private func testURLSessionTask(urlString: String) {
         guard let url = URL(string: urlString) else {
             return
@@ -53,31 +53,33 @@ class ViewController: UIViewController {
 // Implement WKNavigationDelegate methods
 extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        let authMethod = challenge.protectionSpace.authenticationMethod
-        let host = challenge.protectionSpace.host
-        guard authMethod == NSURLAuthenticationMethodServerTrust,
-            let serverTrust = challenge.protectionSpace.serverTrust,
-            host == URL(string: targetHost)?.host else {
-                return completionHandler(.performDefaultHandling, nil)
-        }
-
-        if SSLPinningManager().isValid(serverTrust: serverTrust) {
-            let credential: URLCredential = URLCredential(trust: serverTrust)
-            completionHandler(.useCredential, credential)
-        } else {
-            print(challenge.protectionSpace)
-            completionHandler(.cancelAuthenticationChallenge, nil)
+        DispatchQueue.global(qos: .background).async {
+            let authMethod = challenge.protectionSpace.authenticationMethod
+            let host = challenge.protectionSpace.host
+            guard authMethod == NSURLAuthenticationMethodServerTrust,
+                let serverTrust = challenge.protectionSpace.serverTrust,
+                host == URL(string: self.targetHost)?.host else {
+                    return completionHandler(.performDefaultHandling, nil)
+            }
+            
+            if SSLPinningManager().isValid(serverTrust: serverTrust) {
+                let credential: URLCredential = URLCredential(trust: serverTrust)
+                completionHandler(.useCredential, credential)
+            } else {
+                print(challenge.protectionSpace)
+                completionHandler(.cancelAuthenticationChallenge, nil)
+            }
         }
     }
-                        
+    
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         print("Page load started")
     }
-
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("Page load finished")
     }
-
+    
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         print("Page load failed with error: \(error.localizedDescription)")
     }
