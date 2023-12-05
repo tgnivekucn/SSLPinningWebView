31364f88fd1ae8bff054c248339c983ed28acf5b
Kuang-Yu <kuangyu.nien@somnics.com>
2023-12-05 16:22:44 +0800

    first commit

---
+++ SSLPinningWebView/SSLPinningManager.swift
@@ -0,0 +1,75 @@
+//
+//  SSLPinningManager.swift
+//  SSLPinningWebView
+//
+//  Created by SomnicsAndrew on 2023/12/5.
+//
+
+
+import Foundation
+
+class SSLPinningManager: NSObject, URLSessionDelegate {
+    private var pinnedCertificates: [Data] = []
+
+    override init() {
+        super.init()
+        if let googleCertificate = getLocalGoogleCertificate() {
+            self.pinnedCertificates.append(googleCertificate)
+        }
+        
+        if let mediumCertificate = getLocalMediumCertificate() {
+            self.pinnedCertificates.append(mediumCertificate)
+        }
+    }
+
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        guard challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+              let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        // 驗證伺服器憑證是否匹配
+        if isValid(serverTrust: serverTrust) {
+            completionHandler(.useCredential, URLCredential(trust: serverTrust))
+        } else {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+        }
+    }
+
+    func isValid(serverTrust: SecTrust) -> Bool {
+        let policy = SecPolicyCreateSSL(true, nil)
+        SecTrustSetPolicies(serverTrust, policy)
+        
+        var error: CFError?
+        let result = SecTrustEvaluateWithError(serverTrust, &error)
+        
+        guard result else { return false }
+
+        guard let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else { return false }
+        let serverCertificateData = SecCertificateCopyData(serverCertificate) as Data
+
+        // 比較伺服器憑證和本地打包的憑證
+        return pinnedCertificates.contains(serverCertificateData)
+    }
+
+    private func getLocalGoogleCertificate() -> Data? {
+        if let url = Bundle.main.url(forResource: "_.google.com", withExtension: "cer") {
+            print("url is: \(url.absoluteString)")
+            if let certData = try? Data(contentsOf: url) {
+                return certData
+            }
+        }
+        return nil
+    }
+
+    private func getLocalMediumCertificate() -> Data? {
+        if let url = Bundle.main.url(forResource: "medium.com", withExtension: "cer") {
+            print("url is: \(url.absoluteString)")
+            if let certData = try? Data(contentsOf: url) {
+                return certData
+            }
+        }
+        return nil
+    }
+}
---
+++ SSLPinningWebView/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  SSLPinningWebView
+//
+//  Created by SomnicsAndrew on 2023/12/5.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mWebView: WKWebView!
+    private var targetHost: String {
+        return targetHost2
+    }
+    private let targetHost1 = "https://www.google.com"
+    private let targetHost2 = "https://medium.com"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        testURLSessionTask(urlString: targetHost)
+
+        showWebPage(urlString: targetHost)
+    }
+    
+    private func showWebPage(urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            mWebView.load(request)
+            mWebView.navigationDelegate = self
+        }
+    }
+    
+    private func testURLSessionTask(urlString: String) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let session = URLSession(configuration: .default, delegate: SSLPinningManager(), delegateQueue: nil)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                // 成功響應
+                print("Response success!!")
+            } else {
+                // 失敗響應
+                print("Response fail :(")
+            }
+        }
+        task.resume()
+    }
+}
+
+// Implement WKNavigationDelegate methods
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        let authMethod = challenge.protectionSpace.authenticationMethod
+        let host = challenge.protectionSpace.host
+        guard authMethod == NSURLAuthenticationMethodServerTrust,
+            let serverTrust = challenge.protectionSpace.serverTrust,
+            host == targetHost else {
+                return completionHandler(.performDefaultHandling, nil)
+        }
+
+        if SSLPinningManager().isValid(serverTrust: serverTrust) {
+            let credential: URLCredential = URLCredential(trust: serverTrust)
+            completionHandler(.useCredential, credential)
+        } else {
+            print(challenge.protectionSpace)
+            completionHandler(.cancelAuthenticationChallenge, nil)
+        }
+    }
+                        
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Page load started")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Page load finished")
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("Page load failed with error: \(error.localizedDescription)")
+    }
+}
+
